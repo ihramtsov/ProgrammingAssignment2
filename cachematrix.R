6f82a43f0d84336074cd5f08e87408fb7d0e035e
ihramtsov <ihramtsov@gmail.com>
2015-02-16 22:41:24 +0500

    inverse matrix calculation with cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##function-constructor for matrix cache
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseMatrix <- NULL
+    
+    list(
+            setMatrix = function(newMatrix){
+                x <<- newMatrix
+                inverseMatrix <<- NULL
+            },
+            getMatrix = function() x,
+            getInverseMatrix = function() inverseMatrix,
+            setInverseMatrix = function(inverseMatrixValue){
+                inverseMatrix <<- inverseMatrixValue
+            }
+        )
 }
 
-
-## Write a short comment describing this function
-
+##calculate inverse matrix, get inverted matrix from the cache if any, overwise calculate it and cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverseMatrix <- x$getInverseMatrix()
+    if (!is.null(inverseMatrix)){
+        message("get inverse matrix from cache")
+        return(inverseMatrix)
+    }
+    
+    matrix <- x$getMatrix()
+    inverseMatrix <- calcInverseMatrix(matrix, ...)
+    x$setInverseMatrix(inverseMatrix)
+    inverseMatrix    
+}
+
+##calculate inverse matrix with validation: only square non-singular two-dimentional matrix is acceptable
+##terminate an execution if validation failed
+calcInverseMatrix <- function(m, ...){
+    if (!is.matrix(m) || (dim(m) != 2) || (nrow(m) != ncol(m)))
+        stop("provide square two-dimentional matrix, please")
+    
+    if (det(m) == 0)
+        stop("singular matrix was provided")
+        
+    solve(m)
 }
